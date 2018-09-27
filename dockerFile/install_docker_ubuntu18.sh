792d4d8dbded1a3139fd7a1d653a1ca780bff232
root <root@ip-172-31-18-1.ap-northeast-2.compute.internal>
2018-09-27 06:28:37 +0000

    update docker install shell file

--- dockerFile/install_docker_ubuntu18.sh
+++ dockerFile/install_docker_ubuntu18.sh
@@ -1,5 +1,10 @@
-mkdir -p /engn/docker/ubuntu18 && cd /engn/docker/ubuntu18;
+#!/bin/bash
+
+mkdir -p /engn/docker/ubuntu18;
+cd /engn/docker/ubuntu18;
+
 wget https://download.docker.com/linux/static/stable/x86_64/docker-18.06.1-ce.tgz;
-tar xvf ./docker-18.06.l-ce.tgz;
-rm /usr/bin/docker && ln -s /engn/docker/ubuntu18/docker /usr/bin/docker;
+tar xvf ./docker-18.06.1-ce.tgz -C /engn/docker/ubuntu18;
+rm /usr/bin/docker;
+ln -s /engn/docker/ubuntu18/docker/docker /usr/bin/docker;
 docker -v
